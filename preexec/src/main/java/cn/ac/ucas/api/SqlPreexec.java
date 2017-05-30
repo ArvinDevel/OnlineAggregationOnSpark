fe239a4356e3dbdc3d947623abb5a9abb22b6570
hengyicai <hengyicai@hotmail.com>
2017-05-30 10:45:32 +0800

    Update SqlPreexec

---
+++ preexec/src/main/java/cn/ac/ucas/api/SqlPreexec.java
@@ -0,0 +1,45 @@
+package cn.ac.ucas.api;
+
+import cn.ac.ucas.operator.Operator;
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by caihengyi on 28/05/2017.
+ */
+public class SqlPreexec {
+    private final static Logger logger = Logger.getLogger(SqlPreexec.class);
+
+    private String sql;
+
+    public SqlPreexec(String sql) {
+        this.sql = sql;
+    }
+
+    private boolean validation(){
+        try {
+            Statement stat = CCJSqlParserUtil.parse(this.sql);
+            if(stat instanceof Select){
+                return true;
+            }
+        } catch (JSQLParserException e) {
+            e.printStackTrace();
+            logger.error("Sql invalid!");
+            return false;
+        }
+        logger.error("Only support SELECT!");
+        return false;
+    }
+
+    public Operator parse(){
+        if(validation()) {
+
+            return null;
+        }
+        return null;
+    }
+
+}
